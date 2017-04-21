569cc901583516fab62df9d104eee7c50cef014c
benwh1te <benwh1te@vt.edu>
2017-04-20 21:17:35 -0400

    javascript upload

---
+++ tictactoe.js
@@ -0,0 +1,24 @@
+//tic tac toe in javascript
+
+function movesLeft(board)
+{
+	for(var i = 0; i < 3; i++)
+	{
+		for(var j = 0; j < 3; j++)
+		{
+
+		}
+	}
+}
+
+function eval(board)
+{
+	for(var row = 0; row < 3; row++)
+	{
+
+	}
+	for(var col = 0; col < 3; col++)
+	{
+		
+	}
+}
